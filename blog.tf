b996140963986ebd04aaebb6710daf55fa0ef845
Jakub Sokołowski <jakub@status.im>
2020-01-07 22:59:10 +0100

    point blog.embarklabs.io at GitHub Pages
    Signed-off-by: Jakub Sokołowski <jakub@status.im>

---
+++ blog.tf
@@ -0,0 +1,19 @@
+/* Source of the blog: https://github.com/embarklabs/blog */
+
+locals {
+  github_pages_ips = [
+    "185.199.108.153",
+    "185.199.109.153",
+    "185.199.110.153",
+    "185.199.111.153",
+  ]
+}
+
+resource "cloudflare_record" "blog" {
+  count   = length(local.github_pages_ips)
+  zone_id = local.zones["embarklabs.io"]
+  type    = "A"
+  name    = "blog"
+  value   = local.github_pages_ips[count.index]
+  proxied = true
+}
