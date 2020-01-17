b52624a9d3d56980a2ba59b7cc40ebc061b1fd31
Iuri Matias <iuri.matias@gmail.com>
2020-01-17 17:36:05 -0500

    remove locals

--- subspace.tf
+++ subspace.tf
@@ -1,14 +1,5 @@
 /* Source of the blog: https://github.com/embarklabs/subspace-site */
 
-locals {
-  github_pages_ips = [
-    "185.199.108.153",
-    "185.199.109.153",
-    "185.199.110.153",
-    "185.199.111.153",
-  ]
-}
-
 resource "cloudflare_record" "subspace" {
   count   = length(local.github_pages_ips)
   zone_id = local.zones["embarklabs.io"]
