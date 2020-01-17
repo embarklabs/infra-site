bd7ba236a412204ff80cf5c9b4e77712dd4a0d51
Iuri Matias <iuri.matias@gmail.com>
2020-01-17 18:15:21 -0500

    update main domain (#2)
    * update main domain
    
    * update value in site config

--- redirects.tf
+++ redirects.tf
@@ -5,13 +5,7 @@ locals {
       fqdn = "embark.team",
       dest = "embark.status.im",
       url  = "https://embark.status.im/",
-    },
-    {
-      zone = local.zones["embarklabs.io"],
-      fqdn = "embarklabs.io",
-      dest = "embark.status.im",
-      url  = "https://embark.status.im/",
-    },
+    }
   ]
 }
 
