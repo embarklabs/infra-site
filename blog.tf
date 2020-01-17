4891b1b8ef2357d72b7cd6c59f41358fd8c2b65c
Iuri Matias <iuri.matias@gmail.com>
2020-01-17 18:07:30 -0500

    update main domain

--- blog.tf
+++ blog.tf
@@ -1,14 +1,5 @@
 /* Source of the blog: https://github.com/embarklabs/blog */
 
-locals {
-  github_pages_ips = [
-    "185.199.108.153",
-    "185.199.109.153",
-    "185.199.110.153",
-    "185.199.111.153",
-  ]
-}
-
 resource "cloudflare_record" "blog" {
   zone_id = local.zones["embarklabs.io"]
   type    = "CNAME"
